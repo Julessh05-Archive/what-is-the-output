0fac45801907393733b7f895daf6250f62833076
Julian Schumacher <Jules.media@outlook.de>
2022-06-06 19:58:28 +0200

    New Challenge

---
+++ code/dates/06_06_2022.dart
@@ -0,0 +1,11 @@
+library what_is_the_output;
+// Challenge for 06.06.2022
+
+/// Output Method
+/// Call this to show the Output
+void output() {
+  String _output = '';
+  _output += 'Hello';
+  _output += 'User';
+  print(_output);
+}
--- code/main.dart
+++ code/main.dart
@@ -1,7 +1,7 @@
 library what_is_the_output;
 
-/// Import for 03.05.2022
-import '03_05_2022.dart';
+/// Import for 06.06.2022
+import 'dates/06_06_2022.dart';
 
 /// The Standart Dart Main Method
 void main(List<String> args) {
