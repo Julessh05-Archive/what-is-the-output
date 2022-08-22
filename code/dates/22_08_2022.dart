25a7f602e269744df5663d67c5d3817a0b1b2e9c
Julian Schumacher <Jules.media@outlook.de>
2022-08-22 21:33:16 +0200

    New Challenge

---
+++ code/dates/22_08_2022.dart
@@ -0,0 +1,11 @@
+library what_is_the_output;
+
+// Challenge for 22.08.2022
+
+/// Output Method
+/// Call this to show the Output
+void output() {
+  for (int counter = 0; counter < 3; counter++) {
+    print('Count: $counter');
+  }
+}
--- code/main.dart
+++ code/main.dart
@@ -1,7 +1,7 @@
 library what_is_the_output;
 
-/// Import for 06.06.2022
-import 'dates/06_06_2022.dart';
+/// Import for 22.08.2022
+import 'dates/22_08_2022.dart';
 
 /// The Standart Dart Main Method
 void main(List<String> args) {
